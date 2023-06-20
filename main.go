caa6c5f325df2a28bca84fc8a378b2d5adb9916b
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-06-20 14:26:54 +0900

    feat: create a resource

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"os"
 	"os/signal"
 
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 // newExporter returns a console exporter
@@ -20,6 +23,20 @@ func newExporter(w io.Writer) (trace.SpanExporter, error) {
 		// Do not print timestamps for the demo
 		stdouttrace.WithoutTimestamps(),
 	)
+}
+
+// newResource returns a resource describing this application
+func newResource() *resource.Resource {
+	r, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("fib"),
+			semconv.ServiceVersion("v0.1.0"),
+			attribute.String("environment", "demo"),
+		),
+	)
+	return r
 }
 
 func main() {

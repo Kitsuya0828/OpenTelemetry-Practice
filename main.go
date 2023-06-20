416b1d5d2533b5b193d1aee97234e5a0f39d75e5
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-06-20 14:18:17 +0900

    feat: create a console exporter

--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"os"
 	"os/signal"
+
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
+
+// newExporter returns a console exporter
+func newExporter(w io.Writer) (trace.SpanExporter, error) {
+	return stdouttrace.New(
+		stdouttrace.WithWriter(w),
+		// Use human-readable output
+		stdouttrace.WithPrettyPrint(),
+		// Do not print timestamps for the demo
+		stdouttrace.WithoutTimestamps(),
+	)
+}
 
 func main() {
 	l := log.New(os.Stdout, "", 0)
